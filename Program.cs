94358e4f748a4d23d46c692baf47e5e864d535e3
Ruslan <rkupervas2003@gmail.com>
2024-05-22 10:44:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp2;
+using System;
+
+Salon cars = new Salon();
+bool running = true; 
+
+while(running)
+    Console.WriteLine("Choose the option");
+    Console.WriteLine("1.Add car");
+    Console.WriteLine("2.Remove car");
+string Choice = Console.ReadLine();
+switch (Choice) 
+    {
+        case "1":
+        AddCar(cars);
+            break;
+        case "2":
+        RemoveCar(cars);
+            break;
+    }
+     static void AddCar(Salon cars)
+{
+    Console.WriteLine("Введіть марку авто");
+    string Brand = Console.ReadLine();
+    Console.WriteLine("Введіть модель авто");
+    string Model = Console.ReadLine();
+    Console.WriteLine("Введіть рік виготовлення авто");
+    int Year = Console.ReadLine();
+    
+        static void RemoveCar(Salon cars)
+    {
+    cars.RemoveCar();
+    }
+}
+  
+
+
